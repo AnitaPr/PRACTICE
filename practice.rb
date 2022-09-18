2427f384f936a0b53030b3619f3f1f44b729dac2
AnitaPr <anitabllaca88@gmail.com>
2022-09-17 22:40:52 -0400

    more if/else excercise (3,4)

--- practice.rb
+++ practice.rb
@@ -11,14 +11,31 @@
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-a = 2
+# a = 2
 
-if a < 10
-  p -1 
-elsif a > 10
-  p 1
-elsif a == 10 
-  p 0 
-end 
+# if a < 10
+#   p -1 
+# elsif a > 10
+#   p 1
+# elsif a == 10 
+#   p 0 
+# end 
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+# a = 11
+# b = 9
+
+# if a < 10 && b < 10 
+#   p 1
+# else p 0
+# end 
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
 
+# a = 10000
+# if a > 9000
+#   p 1
+# else p -1
+# end 
