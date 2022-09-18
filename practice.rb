f9157ba11b58e102b1ee2f7e10cd401c8c8803fa
AnitaPr <anitabllaca88@gmail.com>
2022-09-17 22:35:50 -0400

    elsif conditional (excercise 2)

--- practice.rb
+++ practice.rb
@@ -1,9 +1,24 @@
 
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-a = 8
-if a == 10
+# a = 8
+# if a == 10
+#   p 0 
+# else 
+#   p -1
+# end 
+
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+a = 2
+
+if a < 10
+  p -1 
+elsif a > 10
+  p 1
+elsif a == 10 
   p 0 
-else 
-  p -1
 end 
+
+
